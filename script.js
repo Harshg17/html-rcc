37fcba34faa0663cf603803cec79d69ce3d383e7
Harshg17 <142984458+Harshg17@users.noreply.github.com>
2024-09-04 11:35:33 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('add-item-button').addEventListener('click', function() {
-    const list = document.getElementById('item-list');
-    const newItem = document.createElement('li');
-    newItem.textContent = `Item ${list.children.length + 1}`;
-    newItem.classList.add('list-item');
-    list.appendChild(newItem);
-});
